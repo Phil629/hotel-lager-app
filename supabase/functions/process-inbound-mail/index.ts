0161725a5665205bed9a51c9338bd060cc11796a
Philippe <philippe@hotelschottenhof.de>
2026-04-20 11:35:52 +0200

    chore: pin supabase js client version to potentially fix edge function build timeouts

--- supabase/functions/process-inbound-mail/index.ts
+++ supabase/functions/process-inbound-mail/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { encode } from "https://deno.land/std@0.168.0/encoding/base64.ts"
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.3'
 
 // Initialisiere Supabase Client
 const supabaseUrl = Deno.env.get('SUPABASE_URL')!
